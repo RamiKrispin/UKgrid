00f4603c8c9dd389b00a338772f8cac23b22e0f2
RamiKrispin <ramkrisp@umich.edu>
2018-07-30 22:12:21 -0700

    Adding packages reference

--- R/data.R
+++ R/data.R
@@ -26,7 +26,7 @@
 #'
 
 extract_grid <- function(type = "xts", columns = "ND", start = NULL, end = NULL){
-  data(UKgrid)
+  base::data("UKgrid", package = "UKgrid")
 
   # Error handling
   # Checking the values of the columns argument
@@ -80,58 +80,58 @@ extract_grid <- function(type = "xts", columns = "ND", start = NULL, end = NULL)
       }
       # Case date
     } else if(lubridate::is.Date(start)){
-        if(start < base::min(df$TIMESTAMP) | start > max(df$TIMESTAMP)){
+        if(start < base::min(UKgrid$TIMESTAMP) | start > max(UKgrid$TIMESTAMP)){
           warning("The start argument is out of range, using the first available date to filter the data")
-          start_date <- min(df$TIMESTAMP)
+          start_date <- min(UKgrid$TIMESTAMP)
         } else{
-                start_date <- df$TIMESTAMP[which(base::as.Date(df$TIMESTAMP) == start)][1]
+                start_date <- UKgrid$TIMESTAMP[which(base::as.Date(UKgrid$TIMESTAMP) == start)][1]
         }
       # Case POSIXt
     } else if(lubridate::is.POSIXt(start)){
-      if(start < base::min(df$TIMESTAMP) | start > max(df$TIMESTAMP)){
+      if(start < base::min(UKgrid$TIMESTAMP) | start > max(UKgrid$TIMESTAMP)){
         warning("The start argument is out of range, using the first available date to filter the data")
-        start_date <- min(df$TIMESTAMP)
-      } else if(start %in% df$TIMESTAMP){
+        start_date <- min(UKgrid$TIMESTAMP)
+      } else if(start %in% UKgrid$TIMESTAMP){
           start_date <- start
         } else {
-          start_date <- df$TIMESTAMP[which(df$TIMESTAMP > start)[1]]
+          start_date <- UKgrid$TIMESTAMP[which(UKgrid$TIMESTAMP > start)[1]]
           }
     }
   } else {
-    start_date <- min(df$TIMESTAMP)
+    start_date <- min(UKgrid$TIMESTAMP)
   }
 
   # End date
   if(!base::is.null(end)){
     # Case numeric - hence year value
     if(base::is.numeric(end)){
-      if(end < lubridate::year(min(df$TIMESTAMP)) | end > lubridate::year(max(df$TIMESTAMP))){
+      if(end < lubridate::year(base::min(UKgrid$TIMESTAMP)) | end > lubridate::year(base::max(UKgrid$TIMESTAMP))){
         warning("The end argument is out of range, using the last available date to filter the data")
-        end_date <- max(df$TIMESTAMP)
+        end_date <- base::max(UKgrid$TIMESTAMP)
       } else {
-        end_date <- df$TIMESTAMP[which(lubridate::year(df$TIMESTAMP) == end)[length(which(lubridate::year(df$TIMESTAMP) == end))]]
+        end_date <- UKgrid$TIMESTAMP[base::which(lubridate::year(UKgrid$TIMESTAMP) == end)[base::length(base::which(lubridate::year(UKgrid$TIMESTAMP) == end))]]
       }
       # Case date
     } else if(lubridate::is.Date(end)){
-      if(end < base::min(df$TIMESTAMP) | end > max(df$TIMESTAMP)){
+      if(end < base::min(UKgrid$TIMESTAMP) | end > base::max(UKgrid$TIMESTAMP)){
         warning("The end argument is out of range, using the last available date to filter the data")
-        end_date <- base::max(df$TIMESTAMP)
+        end_date <- base::max(UKgrid$TIMESTAMP)
       } else{
-        end_date <- df$TIMESTAMP[which(base::as.Date(df$TIMESTAMP) == end)][length(which(base::as.Date(df$TIMESTAMP) == end))]
+        end_date <- UKgrid$TIMESTAMP[base::which(base::as.Date(UKgrid$TIMESTAMP) == end)][base::length(base::which(base::as.Date(UKgrid$TIMESTAMP) == end))]
       }
       # Case POSIXt
     } else if(lubridate::is.POSIXt(end)){
-      if(end < base::min(df$TIMESTAMP) | end > max(df$TIMESTAMP)){
+      if(end < base::min(UKgrid$TIMESTAMP) | end > base::max(UKgrid$TIMESTAMP)){
         warning("The end argument is out of range, using the last available date to filter the data")
-        end_date <- max(df$TIMESTAMP)
-      } else if(end %in% df$TIMESTAMP){
+        end_date <- base::max(UKgrid$TIMESTAMP)
+      } else if(end %in% UKgrid$TIMESTAMP){
           end_date <- end
         } else {
-        end_date <- df$TIMESTAMP[base::which(df$TIMESTAMP < end)][base::length(base::which(df$TIMESTAMP < end))]
+        end_date <- UKgrid$TIMESTAMP[base::which(UKgrid$TIMESTAMP < end)][base::length(base::which(UKgrid$TIMESTAMP < end))]
       }
     }
   } else {
-    end_date <- max(df$TIMESTAMP)
+    end_date <- base::max(UKgrid$TIMESTAMP)
   }
 
 
@@ -141,8 +141,8 @@ extract_grid <- function(type = "xts", columns = "ND", start = NULL, end = NULL)
     dplyr::filter(TIMESTAMP >= start_date & TIMESTAMP <= end_date)
 
   if(type == "xts"){
-    ts.obj <- xts::xts(df[, which(colnames(df) != "TIMESTAMP") ], order.by = df$TIMESTAMP)
+    ts.obj <- xts::xts(df[, which(colnames(df) != "TIMESTAMP") ], order.by = UKgrid$TIMESTAMP)
   } else if(type == "zoo"){
-    ts.obj <- zoo::zoo(df[, which(colnames(df) != "TIMESTAMP") ], order.by = df$TIMESTAMP)
+    ts.obj <- zoo::zoo(df[, which(colnames(df) != "TIMESTAMP") ], order.by = UKgrid$TIMESTAMP)
   }
 }
