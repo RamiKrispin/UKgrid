83d3823f3c1f35012693e718053d773a6d41acd9
RamiKrispin <ramkrisp@umich.edu>
2019-10-16 17:16:36 -0700

    Add 2019 data

--- data_raw/data_refresh.R
+++ data_raw/data_refresh.R
@@ -1,22 +1,28 @@
 # Load the UK nationalgrid data
 rm(list = ls())
+`%>%` <- magrittr::`%>%`
 df_11_16 <- data.table::fread("https://www.nationalgrid.com/sites/default/files/documents/DemandData_2011-2016.csv")
 df_17 <- data.table::fread("https://www.nationalgrid.com/sites/default/files/documents/DemandData_2017.csv")
-df_18 <- data.table::fread("https://www.nationalgrideso.com/sites/eso/files/documents/DemandData_2018_4.csv")
+# df_18 <- data.table::fread("https://www.nationalgrideso.com/sites/eso/files/documents/DemandData_2018_4.csv")
+df_18 <- data.table::fread("https://demandforecast.nationalgrid.com/efs_demand_forecast/downloadfile?filename=DemandData_2018_1551263484189.csv")
+df_19 <- data.table::fread("https://demandforecast.nationalgrid.com/efs_demand_forecast/downloadfile?filename=DemandData_2019_1567762394330.csv")
 
-df_11_16$SETTLEMENT_DATE <- lubridate::dmy(df_11_16$SETTLEMENT_DATE)
-df_17$SETTLEMENT_DATE <- lubridate::dmy(df_17$SETTLEMENT_DATE)
-df_18$SETTLEMENT_DATE <- lubridate::dmy(df_18$SETTLEMENT_DATE)
+
+df_11_16$SETTLEMENT_DATE <- base::as.Date(lubridate::dmy(df_11_16$SETTLEMENT_DATE, tz = "UTC"), tz = "UTC")
+df_17$SETTLEMENT_DATE <- base::as.Date(lubridate::dmy(df_17$SETTLEMENT_DATE, tz = "UTC"), tz = "UTC")
+df_18$SETTLEMENT_DATE <- base::as.Date(lubridate::dmy(df_18$SETTLEMENT_DATE, tz = "UTC"), tz = "UTC")
+df_19$SETTLEMENT_DATE <- base::as.Date(lubridate::dmy(df_19$SETTLEMENT_DATE, tz = "UTC"), tz = "UTC")
 
 df_11_16$I014_TSD.1 <- NULL
 
-df <- rbind(df_11_16, df_17, df_18)
+df <- rbind(df_11_16, df_17, df_18, df_19)
 
 start_date <- min(df$SETTLEMENT_DATE)
 end_date <- max(df$SETTLEMENT_DATE)
 
 # Filling missing values
-date_vec <- data.frame(SETTLEMENT_DATE = rep(seq.Date(from = start_date, to = end_date, by = "day"), each = 48))
+date_vec <- data.frame(SETTLEMENT_DATE = rep(seq.Date(from = base::as.Date(start_date),
+                                                      to = base::as.Date(end_date), by = "day"), each = 48))
 date_vec$SETTLEMENT_PERIOD <- rep(1:48, times = nrow(date_vec) / 48)
 df1 <- date_vec %>% dplyr::left_join(df)
 
