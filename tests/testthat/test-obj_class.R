feb441a2792b950de2d8fded7ad0829f3f98aabe
RamiKrispin <ramkrisp@umich.edu>
2019-11-27 13:41:39 -0800

    Setting the package testing

--- tests/testthat/test-obj_class.R
+++ tests/testthat/test-obj_class.R
@@ -1,48 +1,39 @@
 context("Test extract_grid function")
 
-test_that(desc = "Test object class",
+test_that(desc = "Test basic object",
           {
-            expect_equal(TRUE, TRUE)
+            x <- UKgrid::extract_grid()
+            expect_equal(tsibble::is_tsibble(x), TRUE)
+            expect_equal(tsibble::index(x) == "TIMESTAMP", TRUE)
+            expect_equal(lubridate::is.POSIXct(x$TIMESTAMP), TRUE)
+          })
+
+
+test_that(desc = "Using arguments - tsibble with hourly aggregation and two variables",
+          {
+            x <- UKgrid::extract_grid(type = "tsibble", columns = c("ND", "TSD"), aggregate = "hourly" )
+            expect_equal(base::all(base::names(x) %in% c("TIMESTAMP", "ND", "TSD")), TRUE)
+            expect_equal(tsibble::is_tsibble(x), TRUE)
+            expect_equal(tsibble::index(x) == "TIMESTAMP", TRUE)
+            expect_equal(lubridate::is.POSIXct(x$TIMESTAMP), TRUE)
+          })
+
+test_that(desc = "Using arguments - xts with monthly aggregation, pulling two variables",
+          {
+            x <- UKgrid::extract_grid(type = "xts", columns = c("ND", "TSD"), aggregate = "daily" )
+            expect_equal(base::all(base::names(x) %in% c("TIMESTAMP", "ND", "TSD")), TRUE)
+            expect_equal(xts::is.xts(x), TRUE)
+            expect_equal(xts::indexClass(x) == "Date", TRUE)
           })
 
 
 
+test_that(desc = "Using arguments - tsibble with monthly aggregation, pulling two variables",
+          {
+            x <- UKgrid::extract_grid(type = "tsibble", columns = c("ND", "TSD"), aggregate = "monthly" )
+            expect_equal(base::all(base::names(x) %in% c("TIMESTAMP", "ND", "TSD")), TRUE)
+            expect_equal(tsibble::is_tsibble(x), TRUE)
+            expect_equal(tsibble::index(x) == "TIMESTAMP", TRUE)
+            expect_equal(all(class(x$TIMESTAMP) %in% c("yearmonth", "Date")), TRUE)
+          })
 
-# Test 1
-
-x <- UKgrid::extract_grid()
-tsibble::is_tsibble(x)
-tsibble::index(x) == "TIMESTAMP"
-lubridate::is.POSIXct(x$TIMESTAMP)
-
-
-x <- UKgrid::extract_grid(type = "tsibble", columns = c("ND", "TSD"), aggregate = "hourly" )
-all(names(x) %in% c("TIMESTAMP", "ND", "TSD"))
-tsibble::is_tsibble(x)
-tsibble::index(x) == "TIMESTAMP"
-lubridate::is.POSIXct(x$TIMESTAMP)
-
-
-x <- UKgrid::extract_grid(type = "tsibble", columns = c("ND", "TSD"), aggregate = "daily" )
-all(names(x) %in% c("TIMESTAMP", "ND", "TSD"))
-tsibble::is_tsibble(x)
-tsibble::index(x) == "TIMESTAMP"
-lubridate::is.Date(x$TIMESTAMP)
-
-x <- UKgrid::extract_grid(type = "xts", columns = c("ND", "TSD"), aggregate = "daily" )
-all(names(x) %in% c("TIMESTAMP", "ND", "TSD"))
-xts::is.xts(x)
-xts::indexClass(x) == "Date"
-
-
-x <- UKgrid::extract_grid(type = "xts", columns = c("ND", "TSD"), aggregate = "monthly" )
-all(names(x) %in% c("TIMESTAMP", "ND", "TSD"))
-xts::is.xts(x)
-xts::indexClass(x) == "Date"
-
-
-x <- UKgrid::extract_grid(type = "tsibble", columns = c("ND", "TSD"), aggregate = "monthly" )
-all(names(x) %in% c("TIMESTAMP", "ND", "TSD"))
-tsibble::is_tsibble(x)
-tsibble::index(x) == "TIMESTAMP"
-all(class(x$TIMESTAMP) %in% c("yearmonth", "Date"))
